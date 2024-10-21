fc44f10428696a9a79b45218d50e32f9adc3c8f3
iqrazartasha33 <iqrazartasha33@gmail.com>
2024-10-21 20:02:29 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+let input = document.getElementById("input-val");
+
+function display(num){
+    input.value += num;
+    input.style.color = "gray";
+}
+
+function clearInput(){
+    input.value = "";
+}
+
+function calculate(){
+    try{
+        input.value = eval(input.value);
+    }
+    catch{
+        input.value = "ERROR";
+        input.style.color = "red"
+        setTimeout(() => {
+         input.value = ""
+        }, 1000)
+    }
+}
+
+
+function del(){
+    input.value = input.value.slice(0 , -1);
+}
+
+
+  
